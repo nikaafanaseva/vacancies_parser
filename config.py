4533680de7528d5299bd3f742e0b66faa3539436
nikaafanaseva <nika.afanaseva@gmail.com>
2026-05-28 01:57:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Settings:
     BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
     MAX_RESULTS: int = int(os.getenv("MAX_RESULTS", "10"))
-    # Источники для поиска
-    ENABLED_SOURCES: list = ["hh.ru", "getmatch.ru", "geekjob.ru"]
+    # Источники для поиска (добавили rabota.ru)
+    ENABLED_SOURCES: list = ["getmatch.ru", "Хабр Карьера", "rabota.ru"]
 
 settings = Settings()
