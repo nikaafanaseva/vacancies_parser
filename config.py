6544b9e68f4629d8cb0e3d3cfda6fe4e034bf6da
nikaafanaseva <nika.afanaseva@gmail.com>
2026-05-27 20:49:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Settings:
-    BOT_TOKEN: str = os.getenv("BOT_TOKEN")
-    FIRECRAWL_API_KEY: str = os.getenv("FIRECRAWL_API_KEY")
-    MAX_RESULTS: int = int(os.getenv("MAX_RESULTS", 5))
-    ENABLED_SOURCES: list = os.getenv("ENABLED_SOURCES", "hh.ru,getmatch.ru,geekjob.ru").split(",")
+    BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
+    FIRECRAWL_API_KEY: str = os.getenv("FIRECRAWL_API_KEY", "")
+
+    MAX_RESULTS: int = int(os.getenv("MAX_RESULTS", "5"))
+    ENABLED_SOURCES: list[str] = [
+        s.strip()
+        for s in os.getenv("ENABLED_SOURCES", "hh.ru,getmatch.ru,geekjob.ru").split(",")
+        if s.strip()
+    ]
+
 
 settings = Settings()
